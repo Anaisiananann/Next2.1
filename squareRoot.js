c6c06eb7a1426de18333937df9c0a41ad30086e0
anaissiananann <anais.thibaudin@gmail.com>
2022-01-11 00:22:01 +0100

    2.3 it comments updated

--- squareRoot.js
+++ squareRoot.js
@@ -1,15 +1,15 @@
 /*
-la racine carrée est le chiffre qui s'il est multiplié par lui meme donne le nombre donné n (racine carré de n)
+la racine carrée est le chiffre qui, s'il est multiplié par lui meme, donne le nombre donné n (racine carré de n)
 la racine carrée est donc inferieure à n
-on doit checker tous les nombre inferieurs à n et voir si en ce multipliant par eux meme il donne n 
-si oui c'est le resultat
+on doit checker tous les nombres inferieurs à n et voir si en ce multipliant par eux meme il donne n 
+si oui, c'est le resultat
 
 ex n =9
 i 1 , i <9
-1*1=1
+1*1=1 !=n
 no return
 i = 2 , i <9
-2*2 = 4
+2*2 = 4 !=n
 no return
 i = 3 , i < 9
 3*3=9=n
